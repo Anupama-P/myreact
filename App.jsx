fabbac59f358c42908292d2e40bc0acc7f9e11e8
Anupama-P <anupamap30@gmail.com>
2016-12-20 12:26:00 +0530

    Files added with simple code to display text

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            My first react project :-)
+         </div>
+      );
+   }
+}
+
+export default App;
